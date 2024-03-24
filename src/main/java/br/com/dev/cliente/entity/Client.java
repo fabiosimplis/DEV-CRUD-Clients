a9d7cfee99efc3665052cf7f3b6c7fd931ecd8b9
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-03-24 16:26:42 -0300

    Add Client entity

---
+++ src/main/java/br/com/dev/cliente/entity/Client.java
@@ -0,0 +1,72 @@
+package br.com.dev.cliente.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String cpf;
+    private Double income;
+
+    //@Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public Client(){}
+
+    public Client(Long id, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+}
