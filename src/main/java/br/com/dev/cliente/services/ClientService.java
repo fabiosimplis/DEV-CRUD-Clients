a8091a450a73f7e509f8dab8f3f8bc7d4b8d7798
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-03-24 19:19:02 -0300

    Add POST Insert Client

--- src/main/java/br/com/dev/cliente/services/ClientService.java
+++ src/main/java/br/com/dev/cliente/services/ClientService.java
@@ -28,4 +28,17 @@ public class ClientService {
         return clients.map(c -> new ClientDTO(c));
 
     }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client client = new Client();
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
+
+        client = repository.save(client);
+        return new ClientDTO(client);
+    }
 }
